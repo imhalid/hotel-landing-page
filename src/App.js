34df38e842c09313c80f25efc82795c4b2626456
imhalid <imhalid@icloud.com>
2022-04-19 18:12:15 +0300

    part three

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Header from "./Components/Header";
 import TopImage from "./Components/TopImage";
 import TopOne from "./Components/TopOne";
 import MidOne from "./Components/MidOne";
+import MidTwo from "./Components/MidTwo";
 
 function App() {
   return (
-    <div className=" ">
-      <div className="absolute overflow-hidden  md:left-1/2 md:transform md:-translate-x-1/2  w-screen md:max-w-[1280px] -z-30 bg-[#F1F6FB] h-[200vh]">
-        <div className=" top-0 w-scrren md:max-w-[1280px] h-[50rem] bg-[#40a2d8] ">
+    <div>
+      <div className="absolute overflow-auto   md:left-1/2 md:transform md:-translate-x-1/2  w-screen md:max-w-[1280px] -z-30 bg-[#F1F6FB] h-full">
+        <div className="top-0 w-scrren md:max-w-[1280px] h-[50rem] bg-[#40a2d8] ">
           <div>
             <Header />
             <TopOne />
@@ -15,6 +16,7 @@ function App() {
           </div>
           <div>
             <MidOne />
+            <MidTwo />
           </div>
         </div>
       </div>
---
+++ src/Components/MidTwo.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
+
+function MidTwo() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-36">
+      <div className="max-w-[250px] text-3xl text-center font-bold">
+        Clean Room, Nice Sweet Dreams.
+      </div>
+      <div className=" space-y-10 md:space-y-0 md:space-x-10 md:flex mt-16">
+        <div className="flex p-10 justify-center items-center flex-col rounded-md w-[347px] h-[363px] bg-white">
+          <BadgeCheckIcon
+            className="bg-[#11768C] w-[5.75rem] p-4 rounded-full"
+            color="white"
+          />
+          <p className="font-bold text-2xl mb-4 mt-14">Great View</p>
+          <p className="leading-7 text-center">
+            The locations is strategist, we have a great view and Lorem ipsum
+            dolor sit amet, consectetur{" "}
+          </p>
+        </div>
+        <div className="flex p-10 justify-center items-center flex-col rounded-md w-[347px] h-[363px] bg-white">
+          <BadgeCheckIcon
+            className="bg-[#11768C] w-[5.75rem] p-4 rounded-full"
+            color="white"
+          />
+          <p className="font-bold text-2xl mb-4 mt-14">Great View</p>
+          <p className="leading-7 text-center">
+            The locations is strategist, we have a great view and Lorem ipsum
+            dolor sit amet, consectetur{" "}
+          </p>
+        </div>
+        <div className="flex p-10 justify-center items-center flex-col rounded-md w-[347px] h-[363px] bg-white">
+          <BadgeCheckIcon
+            className="bg-[#11768C] w-[5.75rem] p-4 rounded-full"
+            color="white"
+          />
+          <p className="font-bold text-2xl mb-4 mt-14">Great View</p>
+          <p className="leading-7 text-center">
+            The locations is strategist, we have a great view and Lorem ipsum
+            dolor sit amet, consectetur{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MidTwo;
